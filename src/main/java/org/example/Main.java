75d8781d6418c86de75e60e9dd2888f315b2a06c
carghai <carghai74@gmail.com>
2023-07-23 17:52:42 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        try {cycledetection.main(args);}
+        catch (Exception ignored) {
+
+        }
+    }
+}
